0be20ee8b907ba1e29367c04d618890d152faf52
Guilherme Freire <guilhermefreire841@gmail.com>
2024-07-12 17:17:54 -0300

    :sparkles: feat: finished layout adjustments

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -17,14 +17,12 @@ export default function Template({ children }: { children: React.ReactNode }) {
       <main className="flex flex-col-reverse sm:flex-row sm:justify-center">
         {!isHome && <Sidebar></Sidebar>}
         <section
-          className={`w-full sm:w-4/5 p-3 ${isHome ? 'h-[88vh]' : 'h-[78vh]'} mr-3 sm:h-[86vh] overflow-y-scroll`}
+          className={`w-full sm:w-4/5 p-3 ${isHome ? 'h-[84vh]' : 'h-[80vh]'} mr-3 sm:h-[86vh] overflow-y-scroll`}
         >
           {children}
         </section>
       </main>
-      <footer
-        className={`${isHome ? 'block' : 'hidden'} absolute bottom-0 px-2 opacity-20`}
-      >
+      <footer className={`${isHome ? 'block' : 'hidden'} px-2 opacity-20`}>
         <small>v1.1.0</small> <small>{parse(new Date().toISOString())}</small>
       </footer>
     </>
